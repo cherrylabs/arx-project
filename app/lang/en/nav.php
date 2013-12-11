5ddf94500e68dd5991373bfe24c456347437c3da
Daniel Sum <daniel@lacerisenumerique.be>
2013-12-11 08:57:31 +0100

    update default lang param

---
+++ app/lang/en/nav.php
@@ -0,0 +1,8 @@
+<?php //
+
+return array(
+    array(
+        'name' => 'Home',
+        'link' => url()
+    )
+);
---
+++ app/lang/en/project.php
@@ -0,0 +1,6 @@
+<?php //
+
+return array(
+    'name' => 'Your project',
+    'link' => url('/')
+);
