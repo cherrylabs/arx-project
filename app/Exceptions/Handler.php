90c86bf4943c880bc762e33f4c2429f122b595ce
Daniel Sum <daniel@lacerisenumerique.be>
2015-05-06 15:02:59 +0200

    - add phpstorm opener by default - add first_name, last_name to user table by default

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,67 +5,86 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler {
 
-	/**
-	 * A list of the exception types that should not be reported.
-	 *
-	 * @var array
-	 */
-	protected $dontReport = [
-		'Symfony\Component\HttpKernel\Exception\HttpException'
-	];
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        'Symfony\Component\HttpKernel\Exception\HttpException'
+    ];
 
-	/**
-	 * Report or log an exception.
-	 *
-	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-	 *
-	 * @param  \Exception  $e
-	 * @return void
-	 */
-	public function report(Exception $e)
-	{
-		return parent::report($e);
-	}
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $e
+     * @return void
+     */
+    public function report(Exception $e)
+    {
+        return parent::report($e);
+    }
 
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $e
-	 * @return \Illuminate\Http\Response
-	 */
-	public function render($request, Exception $e)
-	{
-		if ($this->isHttpException($e))
-		{
-			return $this->renderHttpException($e);
-		}
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $e
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $e)
+    {
+        if ($this->isHttpException($e))
+        {
+            return $this->renderHttpException($e);
+        }
 
 
-		if (config('app.debug'))
-		{
-			return $this->renderExceptionWithWhoops($e);
-		}
+        if (config('app.debug'))
+        {
+            return $this->renderExceptionWithWhoops($e);
+        }
 
-		return parent::render($request, $e);
-	}
+        return parent::render($request, $e);
+    }
 
-	/**
-	 * Render an exception using Whoops.
-	 *
-	 * @param  \Exception $e
-	 * @return \Illuminate\Http\Response
-	 */
-	protected function renderExceptionWithWhoops(Exception $e)
-	{
-		$whoops = new \Whoops\Run;
-		$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+    /**
+     * Render an exception using Whoops.
+     *
+     * @param  \Exception $e
+     * @return \Illuminate\Http\Response
+     */
+    protected function renderExceptionWithWhoops(Exception $e)
+    {
+        $whoops = new \Whoops\Run;
 
-		return new \Illuminate\Http\Response(
-			$whoops->handleException($e),
-			$e->getStatusCode(),
-			$e->getHeaders()
-		);
-	}
+        $handler = new \Whoops\Handler\PrettyPageHandler();
+
+        $translations = array('^' . __DIR__ => '~/Development/PhpStormOpener');
+
+        $handler->setEditor(
+            function ($file, $line) use ($translations) {
+
+                foreach ($translations as $from => $to) {
+                    $file = preg_replace('#' . $from . '#', $to, $file, 1);
+                }
+
+                // return "pstorm://$file:$line"; // old way
+                return "phpstorm://open?file=$file&line=$line"; // as of PhpStorm 8 EAP 138.190+, without my app
+                // "idea://open?file=$file&line=$line"; // alternative way, as of PhpStorm 8 EAP 138.190+, without my app
+
+            }
+        );
+
+        $whoops->pushHandler($handler);
+
+        return new \Illuminate\Http\Response(
+            $whoops->handleException($e),
+            $e->getStatusCode(),
+            $e->getHeaders()
+        );
+    }
 
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('name');
+			$table->string('first_name')->nullable();
+			$table->string('last_name')->nullable();
 			$table->string('email')->unique();
 			$table->string('password', 60);
 			$table->rememberToken();
